e4d729a60ff674a57757056f71523cd1a81d515d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-30 16:48:06 +0100

    add toast after copying

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import toast, { Toaster } from "react-hot-toast";
 import * as L from "react-isloading";
 
 const loaders = [
@@ -14,6 +15,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen w-screen">
+      <Toaster />
       <div
         className={` flex flex-col items-center justify-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-slate-400 rounded-2xl p-8 border border-black font-bold text-lg gap-4 ${
           isCodeVisible ? "animate-reveal" : "animate-hide"
@@ -31,6 +33,7 @@ const App = () => {
             navigator.clipboard.writeText(
               `import { ${name} } from "react-isloading";`
             );
+            toast("Copied to Clipboard!.");
           }}
         >{`import { ${name} } from "react-isloading";`}</code>
         <pre
@@ -48,6 +51,7 @@ const App = () => {
         }}
       />`
             );
+            toast("Copied to Clipboard!.");
           }}
         >
           <code>
