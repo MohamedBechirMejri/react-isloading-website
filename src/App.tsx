208aec12d93e22539dc7c59e4f90637731b844a2
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-30 15:09:09 +0100

    show loader and how to import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
 import React from "react";
-import { L1 } from "react-isloading";
+import * as L from "react-isloading";
 
-function App() {
+const loaders = [
+  {
+    name: "L1",
+    Loader: L.L1,
+  },
+];
+const App = () => {
+  const [name, setName] = React.useState("L1");
+
   return (
     <div className="App">
-      <L1 />
+      <div>
+        <code>{`import { ${name} } from "react-isloading";`}</code>
+        <pre>
+          <code>
+            {`
+                <${name} style={{
+                       height: "15rem",
+                       width: "15rem",
+                       position: "absolute",
+                       top: "50%",
+                       left: "50%",
+                       transform: "translate(-50%, -50%)",
+                 }}/>`}
+          </code>
+        </pre>
+      </div>
+      {loaders.map(({ name, Loader }, i) => (
+        <div
+          key={i}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            margin: "1rem",
+            position: "relative",
+          }}
+          onClick={() => setName(name)}
+        >
+          <Loader
+            style={{
+              height: "15rem",
+              width: "15rem",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          />
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
