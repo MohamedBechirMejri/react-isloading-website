2c4aeb0140b89a83d58e1249185efb5023ad7efa
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-30 16:05:50 +0100

    convert style to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,92 +8,47 @@ const loaders = [
   },
 ];
 
-const codeStyle = {
-  backgroundColor: "#f8f8f8",
-  borderRadius: "0.25rem",
-  // padding: "0.5rem",
-  // margin: "0.5rem",
-  overflow: "auto",
-  fontSize: "0.8rem",
-  fontFamily: "monospace",
-  width: "100%",
-};
-
 const App = () => {
   const [name, setName] = React.useState("L1");
 
   return (
-    <div
-      className="App"
-      style={{
-        minHeight: "100vh",
-        width: "100vw",
-        padding: 0,
-        margin: 0,
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "fixed",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          zIndex: 10,
-        }}
-      >
-        <code
-          style={{
-            ...codeStyle,
-          }}
-        >{`import { ${name} } from "react-isloading";`}</code>
-        <pre
-          style={{
-            ...codeStyle,
-          }}
-        >
+    <div className="min-h-screen w-screen">
+      <div className=" flex flex-col items-center justify-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-slate-400 rounded-2xl p-8 border border-black font-bold text-lg gap-4">
+        <code className=" hover:bg-slate-300 p-4 rounded-2xl text-white w-full transition-all cursor-pointer">{`import { ${name} } from "react-isloading";`}</code>
+        <pre className=" hover:bg-slate-300 p-4 rounded-2xl text-white w-full transition-all cursor-pointer">
           <code>
-            {`
-        <${name} style={{
-                  height: "15rem",
-                  width: "15rem",
-                  position: "absolute",
-                  top: "50%",
-                  left: "50%",
-                  transform: "translate(-50%, -50%)",
-             }}
-         />`}
+            {`<${name} style={{
+        height: "15rem",
+        width: "15rem",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+    }}
+/>`}
           </code>
         </pre>
       </div>
-      {loaders.map(({ name, Loader }, i) => (
-        <div
-          key={i}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            margin: "1rem",
-            position: "relative",
-          }}
-          onClick={() => setName(name)}
-        >
-          <Loader
-            style={{
-              height: "15rem",
-              width: "15rem",
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-            }}
-          />
-        </div>
-      ))}
+      <div className="grid grid-cols-10">
+        {loaders.map(({ name, Loader }, i) => (
+          <div
+            key={i}
+            className="flex flex-col items-center justify-center"
+            onClick={() => setName(name)}
+          >
+            <Loader
+              style={{
+                height: "100%",
+                width: "100%",
+                // position: "absolute",
+                // top: "50%",
+                // left: "50%",
+                // transform: "translate(-50%, -50%)",
+              }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
