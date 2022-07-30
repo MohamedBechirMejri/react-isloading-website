05bbccdb04c5a596b9e7b823b264be1c2f0bb4cc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-30 16:23:43 +0100

    tweak animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,71 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        reveal: "reveal 0.5s ease-in-out forwards",
+        revealPage: "revealPage 0.3s ease-in-out forwards",
+        revealLogo: "revealLogo 0.3s ease-in-out forwards",
+        hide: "hide 0.5s ease-in-out forwards",
+        bounce2: "bounce2 .75s infinite ",
+      },
+      keyframes: {
+        // bounce2: {
+        //   "30%": {
+        //     transform: "translate(0, 100%) scaleY(0.7)",
+        //   },
+        // },
+        reveal: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-50%, -30%)",
+          },
+
+          "70%": {
+            transform: "translate(-50%, -55%)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%, -50%)",
+          },
+        },
+        // revealPage: {
+        //   "0%": {
+        //     opacity: 0,
+        //     transform: "translate(0, 10%)",
+        //   },
+        //   "100%": {
+        //     opacity: 1,
+        //     transform: "translate(0, 0)",
+        //   },
+        // },
+        // revealLogo: {
+        //   "0%": {
+        //     opacity: 0,
+        //     transform: "translate(-5%, 0)",
+        //   },
+        //   "100%": {
+        //     opacity: 1,
+        //     transform: "translate(0, 0)",
+        //   },
+        // },
+        hide: {
+          "0%": {
+            opacity: 1,
+            transform: "translate(-50%, -50%)",
+          },
+
+          "70%": {
+            opacity: 1,
+            transform: "translate(-50%, -55%)",
+          },
+          "100%": {
+            opacity: 0,
+            transform: "translate(-50%, -30%)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
