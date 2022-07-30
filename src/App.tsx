511b87a37de3682b34d4d0002d8a231c3afbc58c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-30 15:39:19 +0100

    add basix styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,65 @@ const loaders = [
     Loader: L.L1,
   },
 ];
+
+const codeStyle = {
+  backgroundColor: "#f8f8f8",
+  borderRadius: "0.25rem",
+  // padding: "0.5rem",
+  // margin: "0.5rem",
+  overflow: "auto",
+  fontSize: "0.8rem",
+  fontFamily: "monospace",
+  width: "100%",
+};
+
 const App = () => {
   const [name, setName] = React.useState("L1");
 
   return (
-    <div className="App">
-      <div>
-        <code>{`import { ${name} } from "react-isloading";`}</code>
-        <pre>
+    <div
+      className="App"
+      style={{
+        minHeight: "100vh",
+        width: "100vw",
+        padding: 0,
+        margin: 0,
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: 10,
+        }}
+      >
+        <code
+          style={{
+            ...codeStyle,
+          }}
+        >{`import { ${name} } from "react-isloading";`}</code>
+        <pre
+          style={{
+            ...codeStyle,
+          }}
+        >
           <code>
             {`
-                <${name} style={{
-                       height: "15rem",
-                       width: "15rem",
-                       position: "absolute",
-                       top: "50%",
-                       left: "50%",
-                       transform: "translate(-50%, -50%)",
-                 }}/>`}
+        <${name} style={{
+                  height: "15rem",
+                  width: "15rem",
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+             }}
+         />`}
           </code>
         </pre>
       </div>
