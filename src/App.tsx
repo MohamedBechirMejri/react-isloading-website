0b990016fecc04e5d9101b387f2aa26c161797be
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-30 12:36:09 +0100

    import first loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { SayHello } from "react-isloading";
+import { L1 } from "react-isloading";
 
 function App() {
   return (
     <div className="App">
-      <h1>{SayHello({ name: "dude" })}</h1>
+      <L1 />
     </div>
   );
 }
